6f6055907b9ab817c5d735a0214989ad41c60157
m.makaev <m.makaev@alef.im>
2021-06-07 15:54:39 +0300

    добавил модели и миграции для времени работы и пауз

---
+++ database/migrations/2021_06_07_114306_create_work_pauseds_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkPausedsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('work_pauseds', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('pause_begin');
+            $table->dateTime('pause_finish');
+            $table->unsignedBigInteger('work_time_id');
+            $table->foreign('work_time_id')->references('id')->on('work_times');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('work_pauseds');
+    }
+}
