2a28cba0b8e725578bbc8106ba0aa68b911b0c6b
m.makaev <m.makaev@alef.im>
2021-06-08 12:48:25 +0300

    добавление тех карт и с ссылкой на категории

--- app/Http/Controllers/GetMapController.php
+++ app/Http/Controllers/GetMapController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
+use App\Models\Categories;
 
 class GetMapController extends Controller
 {
@@ -12,6 +14,21 @@ class GetMapController extends Controller
     {
         $response = Http::withBasicAuth('multishop@4wimax', '3hQ&ue1x')->get('https://online.moysklad.ru/api/remap/1.2/entity/processingplan');
         $data = $response->json();
-        return view('welcome', compact('data'));
+
+        $tech_card = [];
+        for ($i=0; $i<count($data['rows']); $i++) {
+            if (!$data['rows'][$i]['archived']) {
+                $tech_card[] = $data['rows'][$i]['pathName'];
+            }
+
+        }
+        $cat_id =[];
+
+        for ($i=0; $i<count($tech_card); $i++) {
+
+            $cat_id[] = Categories::where('name', $tech_card[$i])->get();
+        }
+
+        return view('welcome', compact('cat_id'));
     }
 }
---
+++ database/seeders/TechnicalCardSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use App\Models\TechnicalCards;
+use App\Models\Categories;
+
+class TechnicalCardSeeder extends Seeder
+{
+
+    /**
+     * @return array
+     */
+    protected function getDataApi(): array
+    {
+        $response = Http::withBasicAuth('multishop@4wimax', '3hQ&ue1x')->get('https://online.moysklad.ru/api/remap/1.2/entity/processingplan');
+        $data = $response->json();
+        $technicalCard = [];
+
+        for ($i=0; $i<count($data['rows']); $i++) {
+            $technicalCard[] = $data['rows'][$i];
+        }
+
+        return $technicalCard;
+    }
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $technicalCard = $this->getDataApi();
+
+
+        for ($i=0; $i<count($technicalCard); $i++) {
+            if (!$technicalCard[$i]['archived']) {
+                $model{$i} = new TechnicalCards();
+                $model{$i}->name = $technicalCard[$i]['name'];
+                $model{$i}->cat_id = Categories::where('name', $technicalCard[$i]['pathName'])->get()[0]->id;
+                $model{$i}->save();
+            }
+        }
+    }
+}
