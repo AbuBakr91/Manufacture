129f2f09feb675e33ee9557b0e3f5345cd615609
m.makaev <m.makaev@alef.im>
2021-06-09 08:05:46 +0300

    Initial commit

--- resources/js/dashboard/pages/UsersList.vue
+++ resources/js/dashboard/pages/UsersList.vue
@@ -18,7 +18,6 @@
                 <td>{{user.lastname}}</td>
                 <td>{{user.slug}}</td>
                 <td class="d-none d-md-table-cell">{{user.email}}</td>
-
                 <td class="table-action">
                     <a @click.prevent="editUser(user.id)" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 align-middle"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg></a>
                     <a @click.prevent="removeUser(user.id)" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash align-middle"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg></a>
